b38776b330424600ef412d25ddeba5c5bbe40d5a
Pawel Brzozowski <pawelbrzozowski91@gmail.com>
2021-10-16 21:04:49 +0200

    Commit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="$emit('btn-click')" :style="{backgroundColor: color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "Default Button",
+    },
+    color: {
+      type: String,
+      default: "rgb(1,1,1)",
+    },
+  },
+  emits:['btn-click']
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @btn-click="$emit('btn-click')" text="Add Task" color='rgb(50, 145, 65)'/>
+  </header>
+</template>
+<script>
+import Button from './Button.vue'
+export default {
+  name: 'Header',
+  components:{
+      Button
+  },
+  props: {
+    title: {
+        type: String,
+        default: 'Default Title'
+    }
+  },
+  emits:['btn-click']
+
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
